45722bbda1d712bc9201a4b91b240960ec57cdc1
Phil <pgrenon.pg@gmail.com>
2021-06-11 23:55:53 -0400

    * Fish1

--- Raymora/Assets/Scripts/Fish.cs
+++ Raymora/Assets/Scripts/Fish.cs
@@ -13,13 +13,13 @@ public class Fish : MonoBehaviour
 
 	private void Awake()
 	{
-		_materials = GetComponentsInChildren<MeshRenderer>().Select(x => x.material).ToArray();
+		_materials = GetComponentsInChildren<SkinnedMeshRenderer>().Select(x => x.material).ToArray();
 		_baseColor = _materials[0].color;
 	}
 
 	private void Update()
 	{
-		transform.RotateAround(Vector3.zero, Vector3.down, _rotationspeed * Time.deltaTime);
+		transform.RotateAround(Vector3.zero, Vector3.up, _rotationspeed * Time.deltaTime);
 	}
 
 	public void TargetOn()
